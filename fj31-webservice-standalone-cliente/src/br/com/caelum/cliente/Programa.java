b8f1da523b57a00826da4d7968f04224325216a1
Fabio Massarotto <fsmassarotto@gmail.com>
2013-10-12 10:52:01 -0300

    Criando um webservice e um client

---
+++ fj31-webservice-standalone-cliente/src/br/com/caelum/cliente/Programa.java
@@ -0,0 +1,10 @@
+package br.com.caelum.cliente;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		CalculadorDeParcela calculador = new CalculadorDeParcelaService().getCalculadorDeParcelaPort();
+		System.out.println(calculador.calculaParcela(100, 2));
+	}
+	
+}
